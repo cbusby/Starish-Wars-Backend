82ddf88223e514614263b2d6577f2868004e5526
John Todd <jtodd@pillartechnology.com>
2018-11-07 14:08:34 -0500

    add a working test

--- internal/swb/persistence/MockPersister.go
+++ internal/swb/persistence/MockPersister.go
@@ -4,12 +4,7 @@ package persistence
 type MockPersister struct {
 }
 
-var savedName string
-var savedContents string
-
 // Save keep function arguments and do nothing
 func (m MockPersister) Save(name string, contents string) error {
-	savedName = name
-	savedContents = contents
 	return nil
 }
