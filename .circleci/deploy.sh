7f34a2777f9997b6369877e56b2008d70672ce63
CBusby <callie_92@att.net>
2018-11-07 16:18:30 -0500

    Setting the region and access in the script rather than circleci settings

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -3,15 +3,22 @@
 set -x
 
 main() {
+    set_up_aws_credentials
     install_aws_cli
     deploy_hello_zip
 }
+
+
+set_up_aws_credentials() {
+  mkdir "${HOME}/.aws"
+  printf "[default]\naws_access_key_id = ${AWS_ACCESS_KEY_ID}\naws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}\nregion = us-east-2" > "${HOME}/.aws/config"
+}
+
 
 deploy_hello_zip() {
     GOOS=linux go build hello.go
     zip hello.zip ./hello
 
-    echo $PWD
     aws lambda update-function-code --function-name AluminumFalcon --zip-file fileb://./hello.zip
 }
 
