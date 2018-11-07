f620ab8b24e9456b2a92d19ffb9982c1f1eda81f
John Todd <jtodd@pillartechnology.com>
2018-11-07 14:59:47 -0500

    add schema validation, but with a hardcoded absolute path to schema

--- internal/swb/processes_test.go
+++ internal/swb/processes_test.go
@@ -1,13 +1,18 @@
 package swb
 
 import (
+	"fmt"
 	"testing"
+
+	"github.com/xeipuuv/gojsonschema"
 
 	"github.com/Starish-Wars-Backend/internal/swb/persistence"
 )
 
 // TestCreate test the Create function
 func TestCreate(t *testing.T) {
+	var schemaLoader = gojsonschema.NewReferenceLoader("file:///Users/johntodd/workspace/go/src/github.com/Starish-Wars-Backend/api/state_schema.json")
+
 	mockPersister := persistence.MockPersister{}
 	gameID, response, err := Create(mockPersister)
 	if err != nil {
@@ -16,7 +21,17 @@ func TestCreate(t *testing.T) {
 	if gameID == "" {
 		t.Errorf("gameID should not be empty")
 	}
-	if response == "" {
-		t.Errorf("response should not be empty")
+	responseLoader := gojsonschema.NewStringLoader(response)
+	result, parseErr := gojsonschema.Validate(schemaLoader, responseLoader)
+	if parseErr != nil {
+		t.Errorf("Did not expect an error while parsing")
+		t.Errorf("%s", parseErr)
+		return
+	}
+	if !result.Valid() {
+		fmt.Printf("The response is not valid. See errors :\n")
+		for _, err := range result.Errors() {
+			fmt.Printf("- %s]n", err)
+		}
 	}
 }
