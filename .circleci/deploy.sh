06cbb7f9e315d6319f2fd44608f2dd49833cf310
CBusby <callie_92@att.net>
2018-11-07 16:30:18 -0500

    Correcting the path to the zip file

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -8,18 +8,18 @@ main() {
     deploy_hello_zip
 }
 
-
 set_up_aws_credentials() {
   mkdir "${HOME}/.aws"
   printf "[default]\naws_access_key_id = ${AWS_ACCESS_KEY_ID}\naws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}\nregion = us-east-2" > "${HOME}/.aws/config"
 }
 
-
 deploy_hello_zip() {
+    pushd hello
     GOOS=linux go build hello.go
     zip hello.zip ./hello
+    popd
 
-    aws lambda update-function-code --function-name AluminumFalcon --zip-file fileb://./hello.zip
+    aws lambda update-function-code --function-name AluminumFalcon --zip-file fileb://./hello/hello.zip
 }
 
 install_aws_cli() {
