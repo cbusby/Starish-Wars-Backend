d3f9de329ff3b1a38f68e6f4ebea93e102d1b91a
John Todd <jtodd@pillartechnology.com>
2019-01-24 15:15:52 -0500

    refactor a little bit

--- internal/swb/validateShipPlacement.go
+++ internal/swb/validateShipPlacement.go
@@ -1,50 +1,58 @@
 package swb
 
 func validateShipPlacement(grid Grid) bool {
-	return allShipsOnBoard(grid) &&
+	return allShipsPresent(grid) &&
 		allShipsHorizontalOrVertical(grid) &&
 		allShipsInTouchingSpaces(grid) &&
 		shipsDoNotOverlap(grid)
 }
 
-func allShipsOnBoard(grid Grid) bool {
-	var retVal = true
+func allShipsPresent(grid Grid) bool {
 	for i := 0; i < len(grid.Carrier); i++ {
 		if grid.Carrier[i] == (Coordinate{}) {
-			retVal = false
+			return false
 		}
 	}
 	for i := 0; i < len(grid.Battleship); i++ {
 		if grid.Battleship[i] == (Coordinate{}) {
-			retVal = false
+			return false
 		}
 	}
 	for i := 0; i < len(grid.Cruiser); i++ {
 		if grid.Cruiser[i] == (Coordinate{}) {
-			retVal = false
+			return false
 		}
 	}
 	for i := 0; i < len(grid.Submarine); i++ {
 		if grid.Submarine[i] == (Coordinate{}) {
-			retVal = false
+			return false
 		}
 	}
 	for i := 0; i < len(grid.Destroyer); i++ {
 		if grid.Destroyer[i] == (Coordinate{}) {
-			retVal = false
+			return false
 		}
 	}
-	return retVal
+	return true
 }
 
 func allShipsHorizontalOrVertical(grid Grid) bool {
-	var retVal = true
-	retVal = retVal && (shipIsHorizontal(grid.Carrier[:]) || shipIsVertical(grid.Carrier[:]))
-	retVal = retVal && (shipIsHorizontal(grid.Battleship[:]) || shipIsVertical(grid.Battleship[:]))
-	retVal = retVal && (shipIsHorizontal(grid.Cruiser[:]) || shipIsVertical(grid.Cruiser[:]))
-	retVal = retVal && (shipIsHorizontal(grid.Submarine[:]) || shipIsVertical(grid.Submarine[:]))
-	retVal = retVal && (shipIsHorizontal(grid.Destroyer[:]) || shipIsVertical(grid.Destroyer[:]))
-	return retVal
+	if !(shipIsHorizontal(grid.Carrier[:]) || shipIsVertical(grid.Carrier[:])) {
+		return false
+	}
+	if !(shipIsHorizontal(grid.Battleship[:]) || shipIsVertical(grid.Battleship[:])) {
+		return false
+	}
+	if !(shipIsHorizontal(grid.Cruiser[:]) || shipIsVertical(grid.Cruiser[:])) {
+		return false
+	}
+	if !(shipIsHorizontal(grid.Submarine[:]) || shipIsVertical(grid.Submarine[:])) {
+		return false
+	}
+	if !(shipIsHorizontal(grid.Destroyer[:]) || shipIsVertical(grid.Destroyer[:])) {
+		return false
+	}
+	return true
 }
 
 func shipIsHorizontal(ship []Coordinate) bool {
