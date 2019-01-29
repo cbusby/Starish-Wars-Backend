89f6bc271c73351cf3283865637d8b7088afcd57
John Todd <jtodd@pillartechnology.com>
2019-01-29 13:27:51 -0500

    add tests for the response processing

--- internal/swb/processes_test.go
+++ internal/swb/processes_test.go
@@ -122,36 +122,121 @@ var _ = ginkgo.Describe("Processes", func() {
 			gomega.Expect(err).NotTo(gomega.BeNil())
 		})
 
-		ginkgo.It("should update player 1's ship positions when provided", func() {
-			newGameBB, _ := ioutil.ReadFile(filepath.Join(examplesDir, "place_ships_request_collapsed.json"))
-			newGameString := string(newGameBB)
-			contents, _ = Update(persister, gameID, newGameString)
-			gomega.Expect(contents).To(gomega.Equal(newGameString))
+		ginkgo.Describe("AWAITING_SHIPS", func() {
+
+			ginkgo.It("should update player 1's ship positions when provided", func() {
+				newGameBB, _ := ioutil.ReadFile(filepath.Join(examplesDir, "place_ships_request_collapsed.json"))
+				newGameString := string(newGameBB)
+				contents, _ = Update(persister, gameID, newGameString)
+				gomega.Expect(contents).To(gomega.Equal(newGameString))
+			})
+
+			ginkgo.It("should update player 2's ship positions and game status when player 1's positions are already provided", func() {
+				oldGameStateBB, _ := ioutil.ReadFile(filepath.Join(examplesDir, "place_ships_request_collapsed.json"))
+				oldGameString := string(oldGameStateBB)
+				persister = persistence.MockPersister{ExpectedGameID: gameID, SavedGameState: oldGameString}
+				newGameBB, _ := ioutil.ReadFile(filepath.Join(examplesDir, "player_2_place_ships_request.json"))
+				newGameString := string(newGameBB)
+				contents, _ := Update(persister, gameID, newGameString)
+				var newGameState model.Game
+				json.Unmarshal([]byte(contents), &newGameState)
+				gomega.Expect(validateShipPlacement(newGameState.Player1.Ships)).To(gomega.BeTrue())
+				gomega.Expect(validateShipPlacement(newGameState.Player2.Ships)).To(gomega.BeTrue())
+				gomega.Expect(newGameState.Status).To(gomega.Equal(model.PLAYER_1_ACTIVE))
+			})
+
+			ginkgo.It("should not change game state or status if the provided position is invalid", func() {
+				oldGameStateBB, _ := ioutil.ReadFile(filepath.Join(examplesDir, "place_ships_request_collapsed.json"))
+				oldGameString := string(oldGameStateBB)
+				persister = persistence.MockPersister{ExpectedGameID: gameID, SavedGameState: oldGameString}
+				newGame := model.Game{Status: model.AWAITING_SHIPS}
+				newGameBB, _ := json.Marshal(newGame)
+				newGameString := string(newGameBB)
+				contents, _ := Update(persister, gameID, newGameString)
+				gomega.Expect(contents).To(gomega.Equal(oldGameString))
+			})
 		})
 
-		ginkgo.It("should update player 2's ship positions and game status when player 1's positions are already provided", func() {
-			oldGameStateBB, _ := ioutil.ReadFile(filepath.Join(examplesDir, "place_ships_request_collapsed.json"))
-			oldGameString := string(oldGameStateBB)
-			persister = persistence.MockPersister{ExpectedGameID: gameID, SavedGameState: oldGameString}
-			newGameBB, _ := ioutil.ReadFile(filepath.Join(examplesDir, "player_2_place_ships_request.json"))
-			newGameString := string(newGameBB)
-			contents, _ := Update(persister, gameID, newGameString)
-			var newGameState model.Game
-			json.Unmarshal([]byte(contents), &newGameState)
-			gomega.Expect(validateShipPlacement(newGameState.Player1.Ships)).To(gomega.BeTrue())
-			gomega.Expect(validateShipPlacement(newGameState.Player2.Ships)).To(gomega.BeTrue())
-			gomega.Expect(newGameState.Status).To(gomega.Equal(model.PLAYER_1_ACTIVE))
-		})
+		ginkgo.Describe("PLAYER_X_ACTIVE", func() {
 
-		ginkgo.It("should not change game state or status if the provided position is invalid", func() {
-			oldGameStateBB, _ := ioutil.ReadFile(filepath.Join(examplesDir, "place_ships_request_collapsed.json"))
-			oldGameString := string(oldGameStateBB)
-			persister = persistence.MockPersister{ExpectedGameID: gameID, SavedGameState: oldGameString}
-			newGame := model.Game{Status: model.AWAITING_SHIPS}
-			newGameBB, _ := json.Marshal(newGame)
-			newGameString := string(newGameBB)
-			contents, _ := Update(persister, gameID, newGameString)
-			gomega.Expect(contents).To(gomega.Equal(oldGameString))
+			var (
+				prev model.Game
+				next model.Game
+			)
+
+			ginkgo.BeforeEach(func() {
+				grid := model.Grid{}
+				grid.Carrier[0] = model.Coordinate{"A", 1}
+				grid.Carrier[1] = model.Coordinate{"A", 2}
+				grid.Carrier[2] = model.Coordinate{"A", 3}
+				grid.Carrier[3] = model.Coordinate{"A", 4}
+				grid.Carrier[4] = model.Coordinate{"A", 5}
+
+				grid.Battleship[0] = model.Coordinate{"A", 6}
+				grid.Battleship[1] = model.Coordinate{"A", 7}
+				grid.Battleship[2] = model.Coordinate{"A", 8}
+				grid.Battleship[3] = model.Coordinate{"A", 9}
+
+				grid.Cruiser[0] = model.Coordinate{"B", 1}
+				grid.Cruiser[1] = model.Coordinate{"B", 2}
+				grid.Cruiser[2] = model.Coordinate{"B", 3}
+
+				grid.Submarine[0] = model.Coordinate{"B", 4}
+				grid.Submarine[1] = model.Coordinate{"B", 5}
+				grid.Submarine[2] = model.Coordinate{"B", 6}
+
+				grid.Destroyer[0] = model.Coordinate{"B", 7}
+				grid.Destroyer[1] = model.Coordinate{"B", 8}
+
+				shots := []model.Coordinate{
+					model.Coordinate{"A", 1},
+				}
+				moreShots := []model.Coordinate{
+					shots[0],
+					model.Coordinate{"A", 2},
+				}
+
+				prev = model.Game{}
+				prev.Player1 = model.Player{Ships: grid, Shots: shots}
+				prev.Player2 = model.Player{Ships: grid, Shots: shots}
+				prev.Status = model.PLAYER_2_ACTIVE
+
+				next = prev
+				next.Player2.Shots = moreShots
+				next.Status = model.PLAYER_1_ACTIVE
+			})
+
+			ginkgo.It("returns new game if validations succeed", func() {
+				oldGameString, _ := marshalGame(prev)
+				persister = persistence.MockPersister{ExpectedGameID: gameID, SavedGameState: oldGameString}
+				newGameString, _ := marshalGame(next)
+				returnedGameString, _ := Update(persister, gameID, newGameString)
+				gomega.Expect(returnedGameString).To(gomega.Equal(newGameString))
+			})
+
+			ginkgo.It("returns old game if validations fail", func() {
+				oldGameString, _ := marshalGame(prev)
+				persister = persistence.MockPersister{ExpectedGameID: gameID, SavedGameState: oldGameString}
+				next.Player2.Ships.Destroyer[1] = model.Coordinate{"J", 10}
+				newGameString, _ := marshalGame(next)
+				returnedGameString, _ := Update(persister, gameID, newGameString)
+				gomega.Expect(returnedGameString).To(gomega.Equal(oldGameString))
+			})
+
+			ginkgo.It("sets game status to GAME_OVER if all ships are sunk", func() {
+				next.Player2.Shots = append([]model.Coordinate(nil), prev.Player1.Ships.Carrier[:]...)
+				next.Player2.Shots = append(next.Player2.Shots, prev.Player1.Ships.Battleship[:]...)
+				next.Player2.Shots = append(next.Player2.Shots, prev.Player1.Ships.Cruiser[:]...)
+				next.Player2.Shots = append(next.Player2.Shots, prev.Player1.Ships.Submarine[:]...)
+				next.Player2.Shots = append(next.Player2.Shots, prev.Player1.Ships.Destroyer[:]...)
+				prev.Player2.Shots = next.Player2.Shots[:len(next.Player2.Shots)-1]
+				oldGameString, _ := marshalGame(prev)
+				persister = persistence.MockPersister{ExpectedGameID: gameID, SavedGameState: oldGameString}
+				newGameString, _ := marshalGame(next)
+				returnedGameString, _ := Update(persister, gameID, newGameString)
+				returnedGame, _ := unmarshalGame(returnedGameString)
+				gomega.Expect(returnedGame.Status).To(gomega.Equal(model.GAME_OVER))
+			})
 		})
 	})
 })
