590a0b3bac356a1fde19d514831904878cbe7b49
John Todd <jtodd@pillartechnology.com>
2019-01-29 11:10:36 -0500

    add more tests

--- internal/swb/validation/validateGameplay_test.go
+++ internal/swb/validation/validateGameplay_test.go
@@ -59,15 +59,65 @@ var _ = ginkgo.Describe("Validation of gameplay", func() {
 		gomega.Expect(ShipsSame(prev.Player1, next.Player1)).To(gomega.BeTrue())
 	})
 
+	ginkgo.It("detects when ships are different from move to move", func() {
+		next.Player1.Ships.Destroyer[1] = model.Coordinate{"C", 7}
+		gomega.Expect(ShipsSame(prev.Player1, next.Player1)).To(gomega.BeFalse())
+	})
+
 	ginkgo.It("successfully detects when inactive player's shot list is the same from move to move", func() {
 		gomega.Expect(ShotsSame(prev.Player1, next.Player1)).To(gomega.BeTrue())
+	})
+
+	ginkgo.It("detects when inactive player's shots are different", func() {
+		next.Player1.Shots = []model.Coordinate{
+			model.Coordinate{"J", 10},
+		}
+		gomega.Expect(ShotsSame(prev.Player1, next.Player1)).To(gomega.BeFalse())
 	})
 
 	ginkgo.It("successfully detects when active player's shot list is the same with one new shot from move to move", func() {
 		gomega.Expect(OneNewShot(prev.Player2, next.Player2)).To(gomega.BeTrue())
 	})
 
+	ginkgo.It("detects when inactive player's and active player's shot lists are the same length", func() {
+		gomega.Expect(OneNewShot(prev.Player1, next.Player1)).To(gomega.BeFalse())
+	})
+
+	ginkgo.It("detects when active player's new shot is off the grid", func() {
+		next.Player2.Shots = []model.Coordinate{
+			prev.Player2.Shots[0],
+			model.Coordinate{"K", 10},
+		}
+		gomega.Expect(OneNewShot(prev.Player2, next.Player2)).To(gomega.BeFalse())
+	})
+
+	ginkgo.It("detects when active player's shot history is different from move to move", func() {
+		next.Player2.Shots = []model.Coordinate{
+			model.Coordinate{"J", 10},
+			model.Coordinate{"A", 2},
+		}
+		gomega.Expect(OneNewShot(prev.Player2, next.Player2)).To(gomega.BeFalse())
+	})
+
+	ginkgo.It("detects when active player's shot history has duplicates", func() {
+		next.Player2.Shots = []model.Coordinate{
+			prev.Player2.Shots[0],
+			prev.Player1.Shots[0],
+		}
+		gomega.Expect(OneNewShot(prev.Player2, next.Player2)).To(gomega.BeFalse())
+	})
+
 	ginkgo.It("correctly detects when all of a player's ships haven't been hit", func() {
 		gomega.Expect(AllShipsHit(next.Player1, next.Player2.Shots)).To(gomega.BeFalse())
+	})
+
+	ginkgo.It("detects when all a player's ships have been hit", func() {
+		next.Player2.Shots = append([]model.Coordinate(nil), prev.Player1.Ships.Carrier[:]...)
+		next.Player2.Shots = append(next.Player2.Shots, prev.Player1.Ships.Battleship[:]...)
+		next.Player2.Shots = append(next.Player2.Shots, prev.Player1.Ships.Cruiser[:]...)
+		next.Player2.Shots = append(next.Player2.Shots, prev.Player1.Ships.Submarine[:]...)
+		next.Player2.Shots = append(next.Player2.Shots, prev.Player1.Ships.Destroyer[:]...)
+
+		gomega.Expect(AllShipsHit(prev.Player1, next.Player2.Shots)).To(gomega.BeTrue())
 	})
 })
